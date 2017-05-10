2962c8a373edbd79c6a71cad5a327ea60d82de8e
Ian Garner and phat nguyen <ian@>
2017-05-10 13:10:34 -0700

    1st commit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// UI
+$(document).ready(function(){
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+    console.log();
+    var firstName =  $("input#new-first-name").val();
+    var lastName = $("input#new-last-name").val();
+    var newStreet = $("input#new-street").val();
+    var newCity = $("input#new-city").val();
+    var newState = $("input#new-state").val();
+
+    var newContact = new Contact(firstName,lastName);
+    $("ul#contacts").append("<li><span>" + "Your first name" + newContact.firstName + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
+
+// UI
+
+//When I have input jerery
+    function Contact(firstName, lastName, newStreet, newCity, newState){
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.newStreet = newStreet;
+      this.newCity = newCity;
+      this.newState = newState;
+    }
+
+
+    // Contact.prototype.fullName(function(){
+    //   return this.firstName + " " + this.lastname
+    // });
